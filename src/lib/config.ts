873f540d5ac5f9e1bb3f8f6862c805a27bd9de97
Dylan Barduzzi <dlbarduzzi@gmail.com>
2026-05-04 14:12:04 -0500

    chore: setup eslint and update boilerplate

---
+++ src/lib/config.ts
@@ -0,0 +1,7 @@
+import { env } from "@/lib/client/env"
+
+export const siteConfig = {
+  url: env.NEXT_PUBLIC_APP_URL,
+  name: "easeona",
+  description: "AI powered interview platform designed to help you prepare with confidence, without the stress.",
+}
